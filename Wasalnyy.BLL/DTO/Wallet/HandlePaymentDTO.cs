b6379bd488370ac3a7ff15b85acac0ff4d324cf7
Mahmud ahmed <mahmud.ahmed.cs@gmail.com>
2025-12-02 22:44:21 +0200

    Refactoring handling Payment Of rider to get the rider id from token

---
+++ Wasalnyy.BLL/DTO/Wallet/HandlePaymentDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wasalnyy.BLL.DTO.Wallet
+{
+    public class HandlePaymentDTO
+    {
+        public string TransactionId { get; set; }
+        public PaymentStatus Status { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
